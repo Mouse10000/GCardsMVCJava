ebd29620f004855b82713f622a477a3633f9ad83
Михаил Варакин <mikhail.varakin.2015@mail.ru>
2025-05-27 00:02:06 +0300

    Clean

--- src/main/java/org/example/dao/CardRepository1.java
+++
@@ -1,8 +0,0 @@
-package org.example.dao;
-
-import org.example.models.Card1;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository1 extends JpaRepository<Card1, Long> {
-}
-
